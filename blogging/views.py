68c357cfab0a1f357bc0d50fc766b8beef39627e
Sean Hodges <sthodges@gmail.com>
2021-03-24 15:39:51 -0700

    Implement list view

--- blogging/views.py
+++ blogging/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from blogging.models import Post
 
 def test_view(request, *args, **kwargs):
     body = "Testing view\n\n"
@@ -8,3 +10,16 @@ def test_view(request, *args, **kwargs):
     if kwargs:
         body += "Kwargs:\n" + "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('blogging/list.html')
+    context = {
+        'posts': posts
+    }
+    body = template.render(context)
+    return HttpResponse(
+        body,
+        content_type='text/html'
+    )
